410e7a01d6497271f496ab8f2d5945d39a536d85
GarrySh <sooo.lame@gmail.com>
2018-05-08 13:10:19 +1000

    change sequelize config

--- app/config/config.js
+++ app/config/config.js
@@ -12,11 +12,6 @@ module.exports = {
   production: {
     dialect: 'postgres',
     use_env_variable: 'DATABASE_URL',
-    // protocol: 'postgres',
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOSTNAME,
     operatorsAliases: false,
   },
 };
